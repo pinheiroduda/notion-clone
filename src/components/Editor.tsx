cd2bc3ca810905451a0caa204a78701debce9818
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2023-11-16 11:26:30 -0300

    feat: adds float menu

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,4 +1,9 @@
-import { useEditor, EditorContent, BubbleMenu } from '@tiptap/react'
+import {
+  useEditor,
+  EditorContent,
+  BubbleMenu,
+  FloatingMenu
+} from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import { initialContent } from '../services/initialContent'
 import { lowlight } from 'lowlight'
@@ -40,6 +45,52 @@ export function Editor() {
         className="max-w-[700px] mx-auto pt-16 prose prose-invert prose-violet"
         editor={editor}
       />
+      {editor && (
+        <FloatingMenu
+          className="bg-zinc-700 py-2 px-1 gap-1 shadow-xl border border-zinc-600 shadow-black/20 rounded-lg overflow-hidden flex flex-col"
+          editor={editor}
+          shouldShow={({ state }) => {
+            const { $from } = state.selection
+
+            const currentLineText = $from.nodeBefore?.textContent
+
+            return currentLineText === '/'
+          }}
+        >
+          <button className="flex items-center gap-2 p-1 rounded min-w-[280px] hover:bg-zinc-600">
+            <img
+              className="w-12 boder boder-zinc-600 rounded"
+              src="https://www.notion.so/images/blocks/text/en-US.png"
+              alt="Text"
+            />
+            <div className="flex flex-col text-left">
+              <span className="text-sm">Text</span>
+              <span className="text-xs text-zinc-400">
+                Just start writing with plain text.
+              </span>
+            </div>
+          </button>
+
+          <button
+            className="flex items-center gap-2 p-1 rounded min-w-[280px] hover:bg-zinc-600"
+            onClick={() =>
+              editor.chain().focus().toggleHeading({ level: 1 }).run()
+            }
+          >
+            <img
+              className="w-12 boder boder-zinc-600 rounded"
+              src="https://www.notion.so/images/blocks/header.57a7576a.png"
+              alt="Heading"
+            />
+            <div className="flex flex-col text-left">
+              <span className="text-sm">Heading 1</span>
+              <span className="text-xs text-zinc-400">
+                Big section heading.
+              </span>
+            </div>
+          </button>
+        </FloatingMenu>
+      )}
       {editor && (
         <BubbleMenu
           className="bg-zinc-700 shadow-xl border border-zinc-600 shadow-black/20 rounded-lg overflow-hidden flex divide-x divide-zinc-600"
